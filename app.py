948ac5fdd5ad81d57e59375fcc6f7aab30a97a19
DDemmer1 <Dennis.Demmer@web.de>
2019-11-05 08:50:34 +0100

    add comments

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import date
 
-app = Flask(__name__)
 
-
+# Article Klasse die die zu scrapenden Daten speichert
 class Article:
     def __init__(self, headline, link, text_body, source, source_name, author, topic, crawl_date, creation_date):
         self.headline = headline
@@ -21,6 +20,8 @@ class Article:
         self.crawl_date = crawl_date
         self.creation_date = creation_date
 
+    # Helfer Methode die es später ermöglicht einen JSON String zu erstellen
+    # siehe return von 'def get_articles()'
     def serialize(self):
         return {
             'headline': self.headline,
@@ -35,20 +36,21 @@ class Article:
         }
 
 
+# Sucht sich die eine Liste mit allen Artikel links zusammen
 def get_news_links(url):
     soup = BeautifulSoup(requests.get(url).content, 'html.parser')
     item = soup.find_all('div', class_='news-header')
 
     links = []
     for item in item:
-        links.append(item.find('a').get('href').strip())
+        links.append(item.find('a').get('href').strip())  # strip damit
     return links
 
 
+# Extrahiert alle notwendigen informationen von einem einzigen Artikel
 def scrape(link):
     soup = BeautifulSoup(requests.get(link).content, 'html.parser')
-    # remove all script tags
-    [s.extract() for s in soup('script')]
+    [s.extract() for s in soup('script')]  # entfernt alle script tags
 
     # HEADLINE
     headline = soup.find('h1', class_='page-heading').string
@@ -65,7 +67,7 @@ def scrape(link):
 
     # TEXT_BODY
     text_body = soup.find_all('div', 'article-container')[0].get_text()
-    text_body = ' '.join(text_body.split())
+    text_body = ' '.join(text_body.split())  # entfernt alle überschüssigen whitespaces und Zeilenumbrüche
 
     # CREATION_DATE
     creation_date = ''
@@ -75,14 +77,28 @@ def scrape(link):
     return Article(headline, link, text_body, 'http://pikio.pl', 'pikio', author, topic, date.today(), creation_date)
 
 
-@app.route('/')
+# ************************* Flask web app *************************  #
+
+
+app = Flask(__name__)
+
+
+# Hier wird der Pfad(route) angegeben der den scraper arbeiten lässt.
+# In dem Fall ist die URL "localhost:5000/pikio"
+@app.route('/pikio')
 def get_articles():
     links = get_news_links('http://pikio.pl')
     articles = []
     for link in links:
         articles.append(scrape(link))
-    return jsonify([e.serialize() for e in articles])
+    return jsonify([e.serialize() for e in articles])  # jsonify erzeugt aus einem Objekt einen String im JSON Format
 
 
+@app.route('/')
+def index():
+    return "<h1>Hier passiert nichts. Bitte gehe zu 'localhost:5000/pikio</h1>"
+
+
+# Web Application wird gestartet
 if __name__ == '__main__':
     app.run()
